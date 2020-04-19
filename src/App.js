874146b7b970d475285ab42f8c561a2f46f61e63
Pulkit Saxena <psaksena85@gmail.com>
2020-04-19 08:28:59 -0400

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {connect} from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -7,14 +8,9 @@ import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			currentUser: null
-		}
-	}
+import {setCurrentUser} from './redux/user/user.actions';
 
+class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
@@ -22,17 +18,13 @@ class App extends React.Component {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 				userRef.onSnapshot(snapShot => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
+					this.props.setCurrentUser({
+						id: snapShot.id,
 							...snapShot.data()
-						}
-					}, () => console.log('In App.js', this.state));
+					});
 				});
 			} else {
-				this.setState({
-					currentUser: userAuth
-				})
+				this.props.setCurrentUser(userAuth);
 			}
 		});
 	}
@@ -44,15 +36,25 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route exact path="/shop" component={ShopPage} />
-					<Route exact path="/signin" component={SignInSignUpPage} />
+					<Route exact path="/signin" render={() => 
+						this.props.currentUser ? (<Redirect to="/" />) : (<SignInSignUpPage />)
+					} />
 				</Switch>
 			</div>
 		);
 	}
 }
 
-export default App;
+const mapStateToProps = ({user}) => ({
+	currentUser: user.currentUser
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-
+  apiKey: "AIzaSyA1-uE4d2CBa3cLYTjvamqc4BTGE01MqNg",
+  authDomain: "crwn-db-151d6.firebaseapp.com",
+  databaseURL: "https://crwn-db-151d6.firebaseio.com",
+  projectId: "crwn-db-151d6",
+  storageBucket: "crwn-db-151d6.appspot.com",
+  messagingSenderId: "717421548338",
+  appId: "1:717421548338:web:a54a89b0e73362c85afd2b",
+  measurementId: "G-W4VMLXXK7X"
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
